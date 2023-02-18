f3507c8946f01d3ff11c2b15a9f49b1017011549
allonmymind <491578923@qq.com>
2023-02-18 23:33:59 +0800

    Create mxq-newinstall.sh

---
+++ armbian/mxq-newinstall.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+#$1=armv7,armv8,aarch64,arm64...
+:<<!
+echo '输入 1 到 4 之间的数字:'
+echo '你输入的数字为:'
+read aNum
+case $aNum in
+    1)  echo '你选择了 1'
+    ;;
+    2)  echo '你选择了 2'
+    ;;
+    3)  echo '你选择了 3'
+    ;;
+    4)  echo '你选择了 4'
+    ;;
+    *)  echo '你没有输入 1 到 4 之间的数字'
+    ;;
+esac
+!
+
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+# 获取当前系统发行版本
+ver=$(lsb_release -d -s)
+oldurl=ports.${ver%% *}.com
+newurl=mirrors.tuna.tsinghua.edu.cn\/${ver%% *}-ports
+echo "####################当前运行的系统为${ver%% *}！！！######################"
+
+
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^1.正在修改时区为东八区^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" > /etc/timezone
+
+echo "####################已修改时区为CST东八区！！！##########################"
+
+
+
+
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^2.换源为国内清华源^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sed -i "s:${oldurl,,}:${newurl,,}:g"  /etc/apt/sources.list
+
+echo "####################已修改为国内清华源！！!##############################"
+
+
+
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^3.正在安装更新^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+apt update && apt upgrade -y
+echo "###############################已完成软件更新！！！####################################"
+
+
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^4.正在安装docker以及docker-compose^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+wget -qO- https://get.docker.com/ | sh && docker --version
+#echo "显示docker版本号代表安装成功！！！"
+echo "##############################docker安装成功！！！######################################"
+
+wget https://ghproxy.com/https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-armv7 -O /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose && docker-compose --version
+#echo "显示docker版本号代表安装成功！！！"
+echo "#############################docker-compose安装成功！！！################################"
+
+#docker镜像加速
+echo "{\"registry-mirrors\":[\"https://hub-mirror.c.163.com/\"]}" >> /etc/docker/daemon.json
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+echo "###############################docker镜像加速设置成功！！！##################################"
+
+
+# 5.创建目录并下载容器配置文件
+mkdir /root/clash && cd /root/clash && wget -O docker-compose.yml https://gh.hangup.ml/allonmymind/HangUp/main/armbian/docker-compose/clash.yml
+
+mkdir /root/wxedge && cd /root/wxedge && wget -O docker-compose.yml https://gh.hangup.ml/allonmymind/HangUp/main/armbian/docker-compose/wxedge.yml
+
+mkdir /root/drpy && cd /root/drpy && wget -O docker-compose.yml https://gh.hangup.ml/allonmymind/HangUp/main/armbian/docker-compose/autodrpy.yml && git clone https://gitcode.net/qq_32394351/dr_py.git
+
+mkdir /root/openwrt && cd /root/openwrt && wget -O docker-compose.yml https://gh.hangup.ml/STIVW/stiv/raw/master/Docker/openwrt/docker-compose.yml
+
+mkdir /root/ddnsgo && cd /root/ddnsgo && wget -O docker-compose.yml https://gh.hangup.ml/allonmymind/HangUp/raw/main/armbian/docker-compose/ddnsgo.yml                
+
+
+
