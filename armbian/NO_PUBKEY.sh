a5407ddd2d4d7a93544d6ef5b18b3a3a61efe436
allonmymind <491578923@qq.com>
2024-08-19 09:54:04 +0800

    Create NO_PUBKEY.sh

---
+++ armbian/NO_PUBKEY.sh
@@ -0,0 +1,1 @@
+sudo apt update 2>&1 1>/dev/null | sed -ne 's/.*NO_PUBKEY //p' | while read key; do if ! [[ ${keys[*]} =~ "$key" ]]; then sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys "$key"; keys+=("$key"); fi; done
